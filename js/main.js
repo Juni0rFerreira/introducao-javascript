0adfaa5b04da3903561904d4604b9972cf567645
Juni0rFerreira <junioredilsonferreira@hotmail.com>
2023-07-22 23:32:26 -0300

    commit ajusto tabela imc

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if(peso <= 0 || peso >= 500){
+        pesoEhValido = false;
+        tdImc.textContent = "Peso invalido!";
+    }
+
+    if(altura <= 0 || altura >= 5.00) {
+        alturaEhValida = false;
+        tdImc.textContent = "Altura invalido!";
+    } 
+
+    if ( alturaEhValida && pesoEhValido ){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
