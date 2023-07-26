cdc904cac820ee8b8d48136e873db5f3d6f5ab89
Juni0rFerreira <junioredilsonferreira@hotmail.com>
2023-07-25 23:44:35 -0300

    commit atualização de formulario de validação

--- js/form.js
+++ js/form.js
@@ -8,8 +8,10 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
         var pacienteTr = montaTr(paciente);
 
-        if(!validaPaciente(paciente)){
-            console.log("Paciente invalido");
+        var erros = validaPaciente(paciente);
+
+        if(erros.length > 0){
+            exibeMensagensDeErro(erros);
             return;
         }
 
@@ -18,7 +20,21 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
         tabela.appendChild(pacienteTr);
 
         form.reset()
+        var mensagensErro = document.querySelector("#mensagens-erro");
+        mensagensErro.innerHTML = "";
 });
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+    
+} 
 
 function obtemPacienteDoFormulario(form){
 
@@ -54,9 +70,32 @@ function montaTd(dado, classe){
 }
 
 function validaPaciente(paciente) {
-    if (validaPeso(paciente.peso)) {
-        return true;
-    } else {
-        return false;
+
+    var erros = [];
+
+    if ( paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco!");
     }
+
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido!");
+    } 
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida!");
+    } 
+
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco!");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco!");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco!");
+    }
+
+    return erros;
 }
